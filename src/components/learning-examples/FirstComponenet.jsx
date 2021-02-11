554d6e6be77b4fd052ce762e59f33127dcaa7fe7
cccmatlock <67501974+cccmatlock@users.noreply.github.com>
2021-02-10 23:18:06 -0600

    First Round of Changes

---
+++ src/components/learning-examples/FirstComponenet.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class FirstComponent extends Component {
+
+    render() {
+        return (
+            <div className="App" >
+                First Component
+            </div>
+        );
+    }
+}
+
+export default FirstComponent;
---
+++ src/components/learning-examples/ThirdComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function ThirdComponent() {
+    return (
+        <div>
+            ThirdComponent
+        </div>
+    );
+}
+
+export default ThirdComponent;
